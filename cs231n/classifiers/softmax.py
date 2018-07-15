f3ee4c089c194e99484eb611324d1a319f04627e
wn172585288 <172585288@qq.com>
2018-07-15 15:08:01 +0800

    first commit

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -30,11 +30,35 @@ def softmax_loss_naive(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   pass
+  num_train=X.shape[0]
+  num_class=W.shape[1]
+  scores=np.dot(X,W)
+  scores_nom=np.zeros((num_train,num_class))
+  for i in xrange(num_train):
+  	scores[i,:]-=np.max(scores[i,:])
+
+  scores_exp=np.exp(scores)
+  for j in xrange(num_train):
+  	scores_nom[j,:]=scores_exp[j,:]/(np.sum(scores_exp[j,:]))
+    
+  scores_nom_correct=scores_nom[np.arange(num_train),y]
+  #scores_nom_correct=np.reshape(scores_nom_correct,(num_train,-1))
+  counts=scores_exp/(scores_exp.sum(axis=1).reshape(num_train,1))
+  counts[range(num_train),y] -= 1
+  dW = np.dot(X.T,counts)
+  loss=-np.log(scores_nom_correct)
+  loss=np.sum(loss)/num_train
+  loss+=0.5*reg*np.sum(W*W)
+  dW=dW/num_train+reg *W
+  
+
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
 
-  return loss, dW
+  return loss,dW
+
 
 
 def softmax_loss_vectorized(W, X, y, reg):
@@ -54,9 +78,28 @@ def softmax_loss_vectorized(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   pass
+  num_train=X.shape[0]
+  num_class=W.shape[1]
+  scores=np.dot(X,W)
+  scores_nom=np.zeros((num_train,num_class))
+  
+  scores_max=np.amax(scores,axis=1)
+  scores_max=np.reshape(scores_max,(num_train,-1))
+  scores_exp=np.exp(scores)
+  scores_nom=scores_exp/np.reshape((np.sum(scores_exp,axis=1)),(num_train,-1))
+  scores_nom_correct=scores_nom[np.arange(num_train),y]
+  
+  counts=scores_exp/(scores_exp.sum(axis=1).reshape(num_train,1))
+  counts[range(num_train),y] -= 1
+  dW = np.dot(X.T,counts)
+
+  loss=-np.log(scores_nom_correct)
+  loss=np.sum(loss)/num_train
+  loss+=0.5*reg*np.sum(W*W)
+  dW=dW/num_train+reg *W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
 
-  return loss, dW
+  return loss,dW
 
